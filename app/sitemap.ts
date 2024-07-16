0cf9e5edb6291e5962e26d52fc4f6bb840c0b3a6
James Spillmann <james@spacinsider.com>
2024-07-15 20:36:05 -0600

    fixing sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,8 @@ import { MetadataRoute } from 'next';
 
 async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://traintimesnyc.com'; // Default to a fixed base URL
-  const domain = `https://${process.env.NEXT_PUBLIC_VERCEL_URL || baseUrl}`;
+  // const domain = `https://${process.env.NEXT_PUBLIC_VERCEL_URL || baseUrl}`;
+  const domain = baseUrl;
 
   const staticPages: MetadataRoute.Sitemap = [
     {
