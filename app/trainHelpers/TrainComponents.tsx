7ab6d3de8e9a16c571d628bdf45a907cd8e63e35
James Spillmann <james@spacinsider.com>
2024-07-16 13:17:14 -0600

    new train colors

--- app/trainHelpers/TrainComponents.tsx
+++ app/trainHelpers/TrainComponents.tsx
@@ -298,139 +298,139 @@ export const TrainMenuBarDesktop: React.FC<TrainMenuBarProps> = ({
 };
 
 export const NComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-yellow-400  transition-all rounded-full">
-    <div className="text-white text-2xl">N</div>
+  <div className="flex items-center justify-center w-8 h-8 bg-[#fbcc0a] transition-all rounded-full">
+    <div className="text-black text-2xl">N</div>
   </div>
 );
 
 export const QComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-yellow-400  transition-all rounded-full">
-    <div className="text-white text-2xl">Q</div>
+  <div className="flex items-center justify-center w-8 h-8 bg-[#fbcc0a] transition-all rounded-full">
+    <div className="text-black text-2xl">Q</div>
   </div>
 );
 
 export const RComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-yellow-400  transition-all rounded-full">
-    <div className="text-white text-2xl">R</div>
+  <div className="flex items-center justify-center w-8 h-8 bg-[#fbcc0a] transition-all rounded-full">
+    <div className="text-black text-2xl">R</div>
   </div>
 );
 
 export const WComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-yellow-400  transition-all rounded-full">
-    <div className="text-white text-2xl">W</div>
+  <div className="flex items-center justify-center w-8 h-8 bg-[#fbcc0a] transition-all rounded-full">
+    <div className="text-black text-2xl">W</div>
   </div>
 );
 
 export const BComponent: React.FC = () => (
-  <div className="flex items-center justify-center  w-8 h-8 bg-orange-400  transition-all rounded-full">
+  <div className="flex items-center justify-center  w-8 h-8 bg-[#ff6318]  transition-all rounded-full">
     <div className="text-white text-2xl">B</div>
   </div>
 );
 
 export const DComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-orange-400  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#ff6318]  transition-all rounded-full">
     <div className="text-white text-2xl">D</div>
   </div>
 );
 
 export const FComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-orange-400  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#ff6318]  transition-all rounded-full">
     <div className="text-white text-2xl">F</div>
   </div>
 );
 
 export const MComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-orange-400  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#ff6318]  transition-all rounded-full">
     <div className="text-white text-2xl">M</div>
   </div>
 );
 
 export const OneComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-red-500  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#ee352e]  transition-all rounded-full">
     <div className="text-white text-2xl">1</div>
   </div>
 );
 
 export const TwoComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-red-500  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#ee352e]  transition-all rounded-full">
     <div className="text-white text-2xl">2</div>
   </div>
 );
 
 export const ThreeComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-red-500  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#ee352e]  transition-all rounded-full">
     <div className="text-white text-2xl">3</div>
   </div>
 );
 
 export const FourComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-green-600  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#00933c]  transition-all rounded-full">
     <div className="text-white text-2xl">4</div>
   </div>
 );
 
 export const FiveComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-green-600  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#00933c]  transition-all rounded-full">
     <div className="text-white text-2xl">5</div>
   </div>
 );
 
 export const SixComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-green-600  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#00933c]  transition-all rounded-full">
     <div className="text-white text-2xl">6</div>
   </div>
 );
 
 export const AComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-cyan-400  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#0139a6]  transition-all rounded-full">
     <div className="text-white text-2xl">A</div>
   </div>
 );
 
 export const CComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-cyan-400  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#0139a6]  transition-all rounded-full">
     <div className="text-white text-2xl">C</div>
   </div>
 );
 
 export const EComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-cyan-400  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#0139a6]  transition-all rounded-full">
     <div className="text-white text-2xl">E</div>
   </div>
 );
 
 export const JComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-yellow-800  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#996633]  transition-all rounded-full">
     <div className="text-white text-2xl">J</div>
   </div>
 );
 
 export const ZComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-yellow-800  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#996633]  transition-all rounded-full">
     <div className="text-white text-2xl">Z</div>
   </div>
 );
 
 export const LComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-slate-400  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#a6a9ac]  transition-all rounded-full">
     <div className="text-white text-2xl">L</div>
   </div>
 );
 
 export const SComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-slate-400  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#a6a9ac]  transition-all rounded-full">
     <div className="text-white text-2xl">S</div>
   </div>
 );
 
 export const SevenComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-violet-400  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#ba33ad]  transition-all rounded-full">
     <div className="text-white text-2xl">7</div>
   </div>
 );
 
 export const GComponent: React.FC = () => (
-  <div className="flex items-center justify-center w-8 h-8 bg-emerald-400  transition-all rounded-full">
+  <div className="flex items-center justify-center w-8 h-8 bg-[#6cbe44]  transition-all rounded-full">
     <div className="text-white text-2xl">G</div>
   </div>
 );
