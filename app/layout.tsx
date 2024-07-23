3c8746fec66572e62c22fd92abb8ec9c6ef81dd0
James Spillmann <james@spacinsider.com>
2024-07-23 13:05:31 -0600

    adding goat counter to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
-import { link } from 'fs';
+import { GCScript } from 'next-goatcounter';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -217,7 +217,10 @@ export default function RootLayout({
         />
       </head>
       <Analytics />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <GCScript siteUrl={'https://traintimes.goatcounter.com/count'} />
+        {children}
+      </body>
     </html>
   );
 }
